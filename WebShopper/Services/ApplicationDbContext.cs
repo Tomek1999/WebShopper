10ad6348b5233cffc336e667b9f5b03dc3ec580c
Tomek <wrona.tomasz33@gmail.com>
2025-03-06 08:20:32 +0100

    Utworzenie migracji klasy waściwości Product, dodanie wartości dla utworzonej tabeli w bazie danych, dodanie katalogu products , który zawiera zdjęcia produktów w formacie .jpg

--- WebShopper/Services/ApplicationDbContext.cs
+++ WebShopper/Services/ApplicationDbContext.cs
@@ -1,6 +1,15 @@
-﻿namespace WebShopper.Services
+﻿using Microsoft.EntityFrameworkCore;
+using WebShopper.Models;
+
+namespace WebShopper.Services
 {
-    public class ApplicationDbContext
+    public class ApplicationDbContext:DbContext
     {
+        public ApplicationDbContext(DbContextOptions options):base(options) { 
+        
+        }
+
+        public DbSet<Product> Products { get; set; }
+
     }
 }
