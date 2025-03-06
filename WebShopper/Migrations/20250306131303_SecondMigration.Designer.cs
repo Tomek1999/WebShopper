c1f82d61d938dd2560630bf2ce3e4dcb865461e7
Tomek <wrona.tomasz33@gmail.com>
2025-03-06 14:37:05 +0100

    Instalacja pakietu entity identity, zdefiniowanie serwisu z rolami użytkowników, dodanie funkcji tworzącej konto administratora po wywołanej przed uruchomieniem aplikacji ( w konsoli)

--- WebShopper/Migrations/20250306131303_SecondMigration.Designer.cs
+++ WebShopper/Migrations/20250306131303_SecondMigration.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using WebShopper.Services;
 
@@ -11,9 +12,11 @@ using WebShopper.Services;
 namespace WebShopper.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250306131303_SecondMigration")]
+    partial class SecondMigration
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
