b181d81687d0f11ae62ac547990daddda3b5773f
Tomek <wrona.tomasz33@gmail.com>
2025-03-06 10:02:34 +0100

    Dodana walidacja wprowadzonych danych produktu, utworzenie obiektu i zapisanie go w bazie , zdjęcie produktu zapisane na serwerze do folderu products, nazwa pliku jest dokładną datą dodania

--- WebShopper/Controllers/ProductsController.cs
+++ WebShopper/Controllers/ProductsController.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using WebShopper.Models;
 using WebShopper.Services;
 
 namespace WebShopper.Controllers
@@ -6,6 +8,8 @@ namespace WebShopper.Controllers
     public class ProductsController : Controller
     {
         private readonly ApplicationDbContext context;
+        private readonly IWebHostEnvironment environment;
+
         public ProductsController(ApplicationDbContext context)
         {
             this.context = context;
@@ -20,6 +24,47 @@ namespace WebShopper.Controllers
         public IActionResult Create() {
             return View();
         }
+        [HttpPost]
+        public IActionResult Create(ProductDto productDto)
+        {
+            if(productDto.ImageFile == null)
+            {
+                ModelState.AddModelError("Zdjęcie", "Dodaj zdjęcie produktu przed zapisaniem");
+            }
+            if (!ModelState.IsValid)
+            {
+                return View(productDto);
+            }
 
-	}
+
+            // save the image file
+            string newFileName = DateTime.Now.ToString("yyyyMMddHHmmssfff");
+            newFileName += Path.GetExtension(productDto.ImageFile!.FileName);
+
+            string imageFullPath = environment.WebRootPath + "/products/" + newFileName;
+            using (var stream = System.IO.File.Create(imageFullPath))
+            {
+                productDto.ImageFile.CopyTo(stream);
+            }
+
+            // save the new product in the database
+            Product product = new Product()
+            {
+                Name = productDto.Name,
+                Brand = productDto.Brand,
+                Category = productDto.Category,
+                Price = productDto.Price,
+                Description = productDto.Description,
+                ImageFileName = newFileName,
+                CreatedAt = DateTime.Now,
+            };
+
+
+            context.Products.Add(product);
+            context.SaveChanges();
+
+            return RedirectToAction("Index", "Products");
+        }
+
+    }
 }
